017d1e1b398c3af6ea905cf8db7b45aaa6cbeb1d
scwanglijun <scwanglijun@gmail.com>
2016-03-17 12:03:28 +0800

    前端页面框架搭建

--- lion-angular-webapp/src/main/webapp/resources/admin/app.js
+++ lion-angular-webapp/src/main/webapp/resources/admin/app.js
@@ -1,3 +1,26 @@
 /**
  * Created by wanglijun on 16/3/15.
  */
+
+var lionApp=angular.module('lionApp',[]);
+//App页面控制
+lionApp.controller('AppController',['$scope',"$rootScope",function($scope){
+
+}]);
+
+//Header Controller
+lionApp.controller('HeaderController',['$scope',function($scope){
+
+}]);
+//LeftMenuController
+lionApp.controller('LeftMenuController',['$scope',function($scope){
+
+}]);
+//PageHeaderController
+lionApp.controller('PageHeadController',['$scope',function($scope){
+
+}]);
+//FooterController
+lionApp.controller('FooterController',['$scope',function($scope){
+
+}]);
