ee223cd803a91398a0cf157dbcecfed80574ce44
maojiawei2012180@126.com <52175lqw>
2016-03-31 13:10:33 +0800

    login 登陆页面

--- lion-angular-webapp/src/main/webapp/html/login.js
+++ lion-angular-webapp/src/main/webapp/html/login.js
@@ -1,40 +1,56 @@
 /**
  * Created by ziv.hung on 16/1/11.
  */
-var loginApp = angular.module('loginApp', []);
-loginApp.controller("loginController", ['$scope', '$http', '$window', LoginController]);
+var loginApp = angular.module('loginApp', [
+    "ui.bootstrap",
+    "dbUtils",
+    "dbImService",
+    "toaster"
+]);
+loginApp.controller("loginController", ['$scope', '$http', '$window','dbUtils', LoginController]);
 
-function LoginController($scope, $http, $window) {
+function LoginController($scope, $http, $window,dbUtils) {
     $scope.data = {
-        userName:'wanglijun',
-        password: '123456',
+        username:'wanglijun',
+        password: '111aaa',
         validCode: "1111"
     };
     //登录请求
     $scope.submitDbForm = function (isValid) {
         $scope.submited = true;
 
-        var userName = $scope.data.userName;
+        var userName = $scope.data.username;
         var password = $scope.data.password;
         var validCode = $scope.data.validCode;
-        if (!userName || !password || !validCode) {
+
+        if (!userName || !password ) {
             alert("登录信息不全,检查后重试!");
             return;
         }
+        console.dir(isValid);
         if (isValid) {
             var reqBody = angular.copy($scope.data);
-            $http.post("../login.do", reqBody).success(function (data) {
-                var errorMsg = data["errorMsg"];
-                if (errorMsg != "") {
-                    $scope.resetVerifyCode();
-                    alert(errorMsg);
-                } else {
-                    $window.sessionStorage.setItem("loginName", userName);
-                    $window.location.href = "index.html";
-                }
-            }).error(function () {
-                alert("系统异常！");
+            dbUtils.post('user.login',reqBody,function(data){
+                console.dir('qqq');
+               if(data.code==='200'){
+                   $window.sessionStorage.setItem("loginName", userName);
+                   $window.location.href = "index.html";
+               } else{
+                   alert(data.message);
+               }
             });
+            //$http.post("../login.do", reqBody).success(function (data) {
+            //    var errorMsg = data["errorMsg"];
+            //    if (errorMsg != "") {
+            //        $scope.resetVerifyCode();
+            //        alert(errorMsg);
+            //    } else {
+            //        $window.sessionStorage.setItem("loginName", userName);
+            //        $window.location.href = "index.html";
+            //    }
+            //}).error(function () {
+            //    alert("系统异常！");
+            //});
         }
     };
 
