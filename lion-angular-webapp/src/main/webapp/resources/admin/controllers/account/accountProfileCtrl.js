59781b57fbde4486f4bb79edb929e9e1cfa1af92
zhongmengdie <mengdie.zhong@newtouch.cn>
2016-04-01 10:36:16 +0800

    个人资料修改

--- lion-angular-webapp/src/main/webapp/resources/admin/controllers/account/accountProfileCtrl.js
+++ lion-angular-webapp/src/main/webapp/resources/admin/controllers/account/accountProfileCtrl.js
@@ -1,10 +1,10 @@
 var DBApp = angular.module('DBApp');
 
 DBApp.controller("accountProfileCtrl", ['$scope','$modal', 'dbUtils',accountProfileCtrl]);
-
 function accountProfileCtrl($scope, $modal, dbUtils) {
+	console.log($scope);
 	dbUtils.post("system.account.profile",{id: '1'},function (data) {
-		alert("pp");
+		alert("kkk");
 	    console.log(data);
 	    });
 }
